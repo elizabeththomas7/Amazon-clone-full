427ae37bc461c6248e5ce18e1e297fd70019acdd
unknown <elizabeththomas92@gmail.com>
2021-02-27 13:52:29 +0530

    first commit

--- src/Provider.js
+++ src/Provider.js
@@ -1,7 +1,9 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
+//data layer
 export const stateContext=createContext();
 
+//enables use of data layer
 export const Provider=({reducer,initialState,children}) =>{
     return (
         <stateContext.Provider value={useReducer(reducer,initialState)}>
